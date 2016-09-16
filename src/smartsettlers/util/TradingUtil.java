d95c0d9540e7f0730f191adf0e2d610b13bb3224
makara_phav <makara.phav@gmail.com>
2016-09-16 18:11:01 +0900

    Trading Utilities implementation for trading consideration and trading options

---
+++ src/smartsettlers/util/TradingUtil.java
@@ -0,0 +1,19 @@
+package smartsettlers.util;
+
+public class TradingUtil {
+	
+	private int[] trad;
+	private int[] state;
+	
+	public TradingUtil(int[] s,int[] trad){
+		this.state = s;
+		this.trad = trad;
+	}
+	//TODO: consider trading offer with simulation
+	public boolean considerOffer(int[] state_change, int orignalStateChance){
+		boolean trad = false;
+		
+		return trad;
+	}
+	
+}
